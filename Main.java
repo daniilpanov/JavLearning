af3e3250a202363c01a21f242b037209a38ef32b
daniilpanov <daniil_panov2005@mail.ru>
2018-09-21 21:11:55 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package Lessons;
+
+public class Main {
+
+	public Main() {
+		
+	}
+	
+	private void checkKeyKode() {
+		
+	}
+	
+	public static void main(String[] argv) {
+		Main main = new Main();
+		main.checkKeyKode();
+	}
+}
